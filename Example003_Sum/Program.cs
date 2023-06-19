22d10989cdd7718c46548f549fb152f7da7a70f3
Konstantin Dmitriev <dmitriev.yurga@gmail.com>
2023-06-20 00:10:15 +0700

    add random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Программа сложения двух чисел которые вводит пользователь
-int namberA = 5;
-int namberB = 6;
+int namberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(namberA);
+int namberB = new Random().Next(1, 10);;
+Console.WriteLine(namberB);
 int sum = namberA+namberB;
 Console.WriteLine(sum);
