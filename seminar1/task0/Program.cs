f2f03dd198ae772b0d39a1cacec15b6036b149a5
Konstantin Dmitriev <dmitriev.yurga@gmail.com>
2023-06-21 01:10:13 +0700

    add task0 квадрат числа

---
+++ seminar1/task0/Program.cs
@@ -0,0 +1,6 @@
+﻿// Написать программу которая принимает число и выдает его квадрат
+Console.Write("Введите число: ");
+string nuberString = Console.ReadLine();
+int number = Convert.ToInt32(nuberString);
+Console.Write("Квадрат числа: ");
+Console.Write(number*number);
