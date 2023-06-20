47fbec77dbee750a3e132c16dd4ccd3223c02c6e
Konstantin Dmitriev <dmitriev.yurga@gmail.com>
2023-06-21 01:32:34 +0700

    add task1 программа проверки является ли число 2 квадратом числа 1

---
+++ seminar1/task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу которая принимает число и выдает его квадрат
+Console.Write("Введите число 1: ");
+string nuberString1 = Console.ReadLine();
+int number1 = Convert.ToInt32(nuberString1);
+Console.Write("Введите число 2: ");
+string nuberString2 = Console.ReadLine();
+int number2 = Convert.ToInt32(nuberString2);
+
+if (number2 * number2 == number1)
+{
+    Console.Write("Число 1 явлется квадратома числа 2");
+}
+else
+{
+    Console.Write("УВЫ, число 1 не явлется квадратома числа 2");
+}
