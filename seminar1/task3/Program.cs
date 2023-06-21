dbfcb778c52c1375c96c8d165ab5cdd5de17c807
Konstantin Dmitriev <dmitriev.yurga@gmail.com>
2023-06-21 12:50:06 +0700

    add task3 программа вывода диапазона чисел

---
+++ seminar1/task3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа которая принимает цело число и выводит диапазон чисел от этого числа с отрицательным знаком до самого числа
+Console.Write("Введите целое число: ");
+string str = Console.ReadLine();
+int chislo = Convert.ToInt32(str);
+for (int i = -chislo; i<=chislo; i++)
+{
+    Console.Write(i);
+    Console.Write(" ");
+}
+
