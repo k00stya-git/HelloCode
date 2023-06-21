3d70ddb26c48bf569a767eab9fbb7baaacd00171
Konstantin Dmitriev <dmitriev.yurga@gmail.com>
2023-06-21 15:01:08 +0700

    Домашние задание 8. Gрограммfу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N

---
+++ seminar1/DZ8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.Write("Введите число N: ");
+string strA = Console.ReadLine();
+int a = Convert.ToInt32(strA);
+int ch = 1;
+Console.Write("Четные числа до N:");
+while (ch <= a)
+{
+    if (ch % 2 == 0)
+    {
+
+        Console.Write($" {ch}");
+    }
+    ch++;
+}
